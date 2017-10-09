7f324fc37bd483ab9e22af3805106ef0f35ff378
diyinqianchang <zhangguolin-1990@163.com>
2017-10-09 20:14:27 +0800

    Initial commit

---
+++ mblog/booktest/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import *
+
+
+#关联对象
+class HeroInfoInline(admin.TabularInline):
+    model = HeroInfo
+    extra = 3
+
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['id','btitle','bpub_date']
+    list_filter = ['btitle']    #列表过滤
+    search_fields = ['btitle']  #以什么字段搜索
+    list_per_page = 1
+    fieldsets = [
+        ('base',{'fields':['btitle']}),
+        ('super',{'fields':['bpub_date']})
+    ]
+
+    inlines = [HeroInfoInline]
+
+admin.site.register(BookInfo,BookInfoAdmin)
+admin.site.register(HeroInfo)
